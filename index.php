4fa9c2edd43c68490abc054eba51efb958998b65
Rachel Dalby <rdalby@hsph.harvard.edu>
2019-02-13 15:01:51 -0500

    added images

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-Test
+    <title> Rachel Dalby </title>
+    <style>
+        img {
+            width: 20%;
+        }
+    </style>
 </head>
+<body>
+    <h1>Rachel Dalby</h1>
+    <h2></h2>
+<br>
+<img src="/Images/IMG_2223.JPG" alt="picture of me"/>
+
+</body>
 </html>
